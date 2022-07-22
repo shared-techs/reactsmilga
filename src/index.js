842b32de07d5d5d3ce627f159012de75095b341a
Peter Rackley <peter@shared-techs.com>
2022-07-22 11:57:07 -0400

    events-complex

--- src/index.js
+++ src/index.js
@@ -45,8 +45,9 @@ const Book = ({ image, title, author }) => {
     console.log(e.target);
     alert('You clicked me!');
   };
-  const complexExample = (author) => {
+  const complexExample = (event, author) => {
     console.log(author);
+    console.log(event.target);
   };
   return (
     <article className='book' onMouseOver={ () => {
@@ -56,7 +57,7 @@ const Book = ({ image, title, author }) => {
       <h1 onClick={ () => console.log(title) }>{ title }</h1>
       <h4 >{ author }</h4>
       <button type='button' onClick={ btnHandler }>Click Me!</button><br />
-      <button type='button' onClick={ () => complexExample(author) }>more complex button</button>
+      <button type='button' onClick={ (event) => complexExample(event, author) }>more complex button</button>
     </article>
   );
 };
