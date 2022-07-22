0ee65eadf0a9be22e9d95f1504709ef9c3da678d
Peter Rackley <peter@shared-techs.com>
2022-07-22 11:24:54 -0400

    props with id

--- src/index.js
+++ src/index.js
@@ -10,16 +10,19 @@ import './index.css';
 // setup vars
 const books = [
   {
+    id: 1,
     image: 'https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg',
     title: 'I Love you to the Moon and Back',
     author: 'Amelia Hepworth'
   },
   {
+    id: 2,
     image: 'https://images-na.ssl-images-amazon.com/images/I/912F2fID5XL._AC_UL200_SR200,200_.jpg',
     title: 'The 6:20 Man',
     author: 'David Balducci'
   },
   {
+    id: 3,
     image: 'https://images-na.ssl-images-amazon.com/images/I/61tqfa+xbWL._AC_UL200_SR200,200_.jpg',
     title: 'Verity',
     author: 'Colleen Hoover'
@@ -29,7 +32,7 @@ const books = [
 function BookList () {
   return (
     <section className='booklist'>
-      { books.map(book => <Book key={ book.title } { ...book } />) }
+      { books.map(book => <Book key={ book.id } { ...book } />) }
     </section>
   );
 };
